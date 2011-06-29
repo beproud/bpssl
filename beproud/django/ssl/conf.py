4bc3ce9a07c47b1e10d1648117a2ad461cfc0ed1
Gabriel Grant <g@briel.ca>
2011-06-28 19:22:50 -0700

    Fixed SSL_REQUEST_HEADER setting to use the custom setting, if available

--- beproud/django/ssl/conf.py
+++ beproud/django/ssl/conf.py
@@ -7,4 +7,7 @@ settings = type('Settings', (object,), {})()
 settings.USE_SSL = getattr(django_settings, "USE_SSL", True)
 settings.SSL_URLS = getattr(django_settings, "SSL_URLS", ())
 settings.SSL_IGNORE_URLS = getattr(django_settings, "SSL_IGNORE_URLS", ())
-settings.SSL_REQUEST_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+settings.SSL_REQUEST_HEADER = getattr(
+    django_settings, "SSL_REQUEST_HEADER",
+    ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+)
