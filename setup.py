2c82ec86580e46462f66af2940f2fbfb6906460c
Gabriel Grant <g@briel.ca>
2011-06-28 19:33:41 -0700

    Bump version and update AUTHORS

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
  
 setup (
     name='bpssl',
-    version='1.0',
+    version='1.0.1',
     description='SSL/HTTPS for Django',
     author='Ian Lewis',
     author_email='ian@beproud.jp',
